11e934a1e0cc42e822d901dbe6a11b4980d5b97a
Will Ware <wware@veracode.com>
2023-10-04 10:35:08 -0400

    BaseModel could use some JSON methods

--- immutability/foo.py
+++ immutability/foo.py
@@ -1,3 +1,5 @@
+import json
+import datetime
 import pytest
 from frozendict import frozendict
 from frozenlist import FrozenList
@@ -40,3 +42,53 @@ def test_frozen_instance_really_frozen():
     with pytest.raises(TypeError):
         p.age = 42
     assert p == Person(name="Bob", age=35)
+
+
+class BaseJsonModel(BaseModel):
+    @classmethod
+    def fromjson(cls, s):
+        d = json.loads(s)
+        return cls(**d)
+
+    def json(self):
+        def make_json_dumpable(obj):
+            if obj is None or isinstance(obj, (int, float)):
+                return obj
+            elif isinstance(obj, complex):
+                return {'re': obj.real, 'im': obj.imag}
+            elif isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
+                return obj.isoformat()
+            elif isinstance(obj, datetime.timedelta):
+                return str(obj)
+            elif isinstance(obj, (set, list, tuple)):
+                return [make_json_dumpable(x) for x in obj]
+            elif isinstance(obj, dict):
+                return dict(make_json_dumpable(list(obj.items())))
+            else:
+                return str(obj)
+        return json.dumps(make_json_dumpable(self.dict()))
+
+
+class Person2(BaseJsonModel, frozen=True):
+    name: str
+    age: int
+    birthday: datetime.date
+
+
+def test_json_frozen_instance_really_frozen():
+    bday = datetime.date(1982, 3, 25)
+    p = Person2(name="Bob", age=35, birthday=bday)
+    with pytest.raises(TypeError):
+        p.name = "Jimmy"
+    with pytest.raises(TypeError):
+        p.age = 42
+    assert p == Person2(name="Bob", age=35, birthday=bday)
+    assert '"name": "Bob"' in p.json(), p.json()
+    assert '"age": 35' in p.json(), p.json()
+    assert '"birthday": "1982-03-25"' in p.json(), p.json()
+    p2 = Person2.fromjson(p.json())
+    assert p2 == p, p2
+    assert p2.name == "Bob", p2
+    assert p2.age == 35, p2
+    assert p2.birthday == bday, p2
+    # TODO also test datetime.datetime, datetime.time, datetime.timedelta
